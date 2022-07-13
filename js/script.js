c782ab1b08b055ac59a3d08caf53a3a335b32363
Alex <mcbuzzer@gmail.com>
2022-07-13 15:45:52 -0600

    Refactored the Theme system, big improvement

--- js/script.js
+++ js/script.js
@@ -1,111 +1,100 @@
+let themeDict = {
+    "blueberry" : {
+        "--primary-color" : "#061A40",
+        "--secondary-color" : "#003559",
+        "--tertiary-color" : "#006DAA",
+        "--accent-color" : "#0353A4",
+        "--text-color" : "#B9D6F2"
+    },
+    "mint" : {
+        "--primary-color" : "#002A32",
+        "--secondary-color" : "#08605f",
+        "--tertiary-color" : "#2EC4B6",
+        "--accent-color" : "#CBF3F0",
+        "--text-color" : "#FFFFFF"
+    },
+    "raisin" : {
+        "--primary-color" : "#201A23",
+        "--secondary-color" : "#2E2532",
+        "--tertiary-color" : "#43344A",
+        "--accent-color" : "#6A477B",
+        "--text-color" : "#f8e5ee"
+    },
+    "island" : {
+        "--primary-color" : "#05668D",
+        "--secondary-color" : "#028090",
+        "--tertiary-color" : "#00A896",
+        "--accent-color" : "#02C39A",
+        "--text-color" : "#F0F3BD"
+    },
+    "pastel" : {
+        "--primary-color" : "#64A6BD",
+        "--secondary-color" : "#90A8C3",
+        "--tertiary-color" : "#ADA7C9",
+        "--accent-color" : "#D7B9D5",
+        "--text-color" : "#F4CAE0"
+    }
+}
+// ^^^ Theme List ^^^
+
+startup();
+
+// vvv Dropdown vvv
 let dropdown = document.getElementById("dropdownToggle");
 
 dropdown.addEventListener("click", evt => {
     document.getElementById("dropContent").classList.toggle("show");
     document.getElementById("dropArrow").classList.toggle("up");
-    console.log("Gluck")
 })
 
-window.onclick = function(event) {
-    if (!event.target.matches(".drop-btn")) {
-        var dropdown = document.getElementById("dropContent");
-        var i;
-        for (i = 0; i < dropdown.length; i++) {
-            var openDropdown = dropdown[i];
-            if (openDropdown.classList.contains("show")) {
-                openDropdown.classList.remove("show")
-            }
+window.onclick = (evt) => {
+    if (!evt.target.matches(".drop-btn") && !evt.target.matches("#title")&& !evt.target.matches(".drop-arrow")) {
+        var dropdownC = document.getElementById("dropContent");
+        var arrow = document.getElementById("dropArrow");
+        if (dropdownC.classList.contains("show")) {
+            dropdownC.classList.remove("show");
+        }
+        if (arrow.classList.contains("up")) {
+            arrow.classList.remove("up");
         }
     }
 }
+
 // ^^^ Dropdown ^^^
 
-let ddBlueberry = document.getElementById("ddBlueberry");
-let ddMint = document.getElementById("ddMint");
-let ddRaisin = document.getElementById("ddRaisin");
-let ddIsland = document.getElementById("ddIsland");
-let ddLesbian = document.getElementById("ddLesbian");
-//Calls theme switch at startup
-dommyMommy();
+function startup() {
+    let connection = document.getElementById("dropContent");
+    for (key in themeDict) {
+        let themeName = key;
+        let item = document.createElement("div");
+        item.id = themeName;
+        item.innerText = themeName.charAt(0).toUpperCase() + themeName.slice(1);
+        item.classList.add("drop-item");
 
-ddBlueberry.addEventListener("click", (evt) => {
-    localStorage.setItem("theme","blueberry");
-    dommyMommy();
-})
+        item.addEventListener("click", (evt) => {
+            localStorage.setItem("theme", item.id);
+            dommyMommy();
+        })
 
-ddMint.addEventListener("click", (evt) => {
-    localStorage.setItem("theme","mint");
-    dommyMommy();
-})
-
-ddRaisin.addEventListener("click", (evt) => {
-    localStorage.setItem("theme","raisin");
-    dommyMommy();
-})
-
-ddIsland.addEventListener("click", (evt) => {
-    localStorage.setItem("theme","island");
-    dommyMommy();
-})
-
-ddLesbian.addEventListener("click", (evt) => {
-    localStorage.setItem("theme","pastel");
-    dommyMommy();
-})
-
-function dommyMommy() {
-    let theme = localStorage.getItem("theme")
-
-    switch (theme) {
-        case "blueberry":
-            document.getElementById("title").innerText = "Blueberry";
-            document.documentElement.style.setProperty("--primary-color", "#061A40");
-            document.documentElement.style.setProperty("--secondary-color", "#003559");
-            document.documentElement.style.setProperty("--tertiary-color", "#006DAA");
-            document.documentElement.style.setProperty("--accent-color", "#0353A4");
-            document.documentElement.style.setProperty("--text-color", "#B9D6F2");
-            break;
-        case "mint":
-            document.getElementById("title").innerText = "Mint";
-            document.documentElement.style.setProperty("--primary-color", "#002A32");
-            document.documentElement.style.setProperty("--secondary-color", "#08605f");
-            document.documentElement.style.setProperty("--tertiary-color", "#2EC4B6");
-            document.documentElement.style.setProperty("--accent-color", "#CBF3F0");
-            document.documentElement.style.setProperty("--text-color", "#FFFFFF");
-            break;
-        case "raisin":
-            document.getElementById("title").innerText = "Raisin";
-            document.documentElement.style.setProperty("--primary-color", "#201A23");
-            document.documentElement.style.setProperty("--secondary-color", "#2E2532");
-            document.documentElement.style.setProperty("--tertiary-color", "#43344A");
-            document.documentElement.style.setProperty("--accent-color", "#6A477B");
-            document.documentElement.style.setProperty("--text-color", "#f8e5ee");
-            break;
-        case "island":
-            document.getElementById("title").innerText = "Island";
-            document.documentElement.style.setProperty("--primary-color", "#05668D");
-            document.documentElement.style.setProperty("--secondary-color", "#028090");
-            document.documentElement.style.setProperty("--tertiary-color", "#00A896");
-            document.documentElement.style.setProperty("--accent-color", "#02C39A");
-            document.documentElement.style.setProperty("--text-color", "#F0F3BD");
-            break;
-        case "pastel":
-            document.getElementById("title").innerText = "Pastel";
-            document.documentElement.style.setProperty("--primary-color", "#64A6BD");
-            document.documentElement.style.setProperty("--secondary-color", "#90A8C3");
-            document.documentElement.style.setProperty("--tertiary-color", "#ADA7C9");
-            document.documentElement.style.setProperty("--accent-color", "#D7B9D5");
-            document.documentElement.style.setProperty("--text-color", "#F4CAE0");
-            break;
-        default:
-            document.getElementById("title").innerText = "Broken";
-            document.documentElement.style.setProperty("--primary-color", "#123456");
-            document.documentElement.style.setProperty("--secondary-color", "#123456");
-            document.documentElement.style.setProperty("--tertiary-color", "#123456");
-            document.documentElement.style.setProperty("--accent-color", "#123456");
-            document.documentElement.style.setProperty("--text-color", "#123456");
-            break;
+        // if (i == (Object.keys(themeDict).length - 1)) {
+        //     item.classList.add("b-bot-rad-20");
+        // }
+        connection.appendChild(item);
     }
+    connection.lastChild.classList.add("b-bot-rad-20");
+    dommyMommy();
+}
+
+
+// localstorage + actual theme execution
+function dommyMommy() {
+    let themeName = localStorage.getItem("theme");
+
+    document.documentElement.style.setProperty("--primary-color", themeDict[themeName]["--primary-color"]);
+    document.documentElement.style.setProperty("--secondary-color", themeDict[themeName]["--secondary-color"]);
+    document.documentElement.style.setProperty("--tertiary-color", themeDict[themeName]["--tertiary-color"]);
+    document.documentElement.style.setProperty("--accent-color", themeDict[themeName]["--accent-color"]);
+    document.documentElement.style.setProperty("--text-color", themeDict[themeName]["--text-color"]);
 }
 
 // ^^^ Themes ^^^
